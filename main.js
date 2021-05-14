5d7e63f5d3e35b22b3c43aba81fba6b27fe57bf9
Edwin Vasquez <edwin.vasquez@grupopdc.com>
2021-05-14 01:16:30 -0600

    Pagina de carretilla con localstorage de los productos agregados, calculos de total por item y cantidad, y suma de subtotales

--- main.js
+++ main.js
@@ -30,6 +30,7 @@ let products = [
 for(let item = 0; item < carts.length; item++){
     carts[item].addEventListener('click', () => {
         cartNumbers(products[item]);
+        totalCost(products[item]);
     });
 }
 
@@ -78,4 +79,53 @@ function setItems(product) {
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
 }
 
+function totalCost(product){
+    let cartCost = localStorage.getItem("totalCost");
+
+    if(cartCost != null){
+        cartCost = parseInt(cartCost);
+        localStorage.setItem("totalCost", cartCost + product.price);
+    } else {
+        localStorage.setItem("totalCost", product.price);
+    }
+}
+
+function displayCart(){
+    let cartItems = localStorage.getItem("productsInCart");
+    cartItems = JSON.parse(cartItems);
+
+    let productContainer = document.querySelector(".products");
+    let cartCost = localStorage.getItem("totalCost");
+
+    if(cartItems && productContainer){
+        productContainer.innerHTML = '';
+        Object.values(cartItems).map(item => {
+            productContainer.innerHTML += `
+                <div class="product">
+                    <ion-icon name="close-circle"></ion-icon>
+                    <img src="./images/${item.tag}.jpg">
+                    <span>${item.name}</span>
+                </div>
+                <div class="price">$${item.price}.00</div>
+                <div class="quantity">
+                    <ion-icon class="decrease" name="caret-back-circle"></ion-icon>
+                    <span>${item.inCart}</span>
+                    <ion-icon class="increase" name="caret-forward-circle"></ion-icon>
+                </div>
+                <div class="total">
+                    $${item.inCart * item.price}.00
+                </div>
+            `
+        });
+
+        productContainer.innerHTML += `
+            <div class="basketTotalContainer">
+                <h4 class="basketTotalTitle">Basket Total</h4>
+                <h4 class="basketTotal">$${cartCost}.00</h4>
+            </div>
+        `
+    }
+}
+
 onLoadCartNumbers();
+displayCart();
